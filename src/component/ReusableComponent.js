2f6014ee7d43c1b8f887431336a248dc9c41e389
Himanshu Rawat <himanshu091rawat@gmail.com>
2021-05-06 13:43:29 +0530

    Basics of components

---
+++ src/component/ReusableComponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ReusableComponent({number}) {
+    return (
+        <div>
+        <h1>Reusable Component {number} </h1>
+        </div>
+    )
+}
