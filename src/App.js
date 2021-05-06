72620a6282bfebfe6481013c5282330021092c84
Himanshu Rawat <himanshu091rawat@gmail.com>
2021-05-06 13:44:02 +0530

    Added temaplate

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import ReusableComponent from './component/ReusableComponent';
 
--- src/component/ReusableComponent.js
+++ src/component/ReusableComponent.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
-export default function ReusableComponent({number}) {
+export default function ReusableComponent({number}) 
+{
     return (
         <div>
         <h1>Reusable Component {number} </h1>
