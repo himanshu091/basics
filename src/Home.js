c03a7fb4bfd0311fde08c7e2f249e881f7d615f3
Himanshu Rawat <himanshu091rawat@gmail.com>
2021-05-10 07:45:24 +0530

    Filter Method

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,19 @@
 import { useState } from 'react';
+import BlogList from './BlogList';
 const Home = () => {
 
   const [blogs, setBlogs] = useState([
     { title: "First Blog", body: "Lorem Ipsum...", author: "Himanshu", id: 1 },
-    { title: "Second Blog", body: "Lorem Ipsum...", author: "Himanshu", id: 2 },
+    { title: "Second Blog", body: "Lorem Ipsum...", author: "John", id: 2 },
     { title: "Third Blog", body: "Lorem Ipsum...", author: "Himanshu", id: 3 }
   ]);
 
   return (
-    <div className="content">
-      <h1>Learn about anonymous function</h1>
-      {/* Function invoked itself */}
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written By : {blog.author}</p>
-        </div>
-      ))}
+    <div className="home">
+     
+
+  <BlogList blogs={blogs} title="All Blogs" />
+  <BlogList blogs={blogs.filter((blog)=>(blog.author==="Himanshu"))} title="Himanshu Blogs" />
 
     </div>
   );
