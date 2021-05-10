021bcdf5e46ac33ba6f36e7591381e002e6a9cac
Himanshu Rawat <himanshu091rawat@gmail.com>
2021-05-10 07:22:52 +0530

    Map function

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,25 @@
-
+import { useState } from 'react';
 const Home = () => {
 
-    const higherOrder = (name)=>{
-        console.log("Your Name is" + name)
-    }
- 
-    return (
-        <div className="content">
-        <h1>Learn about anonymous function</h1>
-        
-        <button onClick={()=>{
-            higherOrder("Himanshu")
-        }} >Click Me </button>
+  const [blogs, setBlogs] = useState([
+    { title: "First Blog", body: "Lorem Ipsum...", author: "Himanshu", id: 1 },
+    { title: "Second Blog", body: "Lorem Ipsum...", author: "Himanshu", id: 2 },
+    { title: "Third Blog", body: "Lorem Ipsum...", author: "Himanshu", id: 3 }
+  ]);
 
+  return (
+    <div className="content">
+      <h1>Learn about anonymous function</h1>
+      {/* Function invoked itself */}
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written By : {blog.author}</p>
         </div>
-     );
+      ))}
+
+    </div>
+  );
 }
- 
+
 export default Home;
