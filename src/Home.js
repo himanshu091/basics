5a896f32259536d25948477ca64d67e234b5f568
Himanshu Rawat <himanshu091rawat@gmail.com>
2021-05-15 08:11:29 +0530

    Added Catching Error

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,9 @@ const Home = () => {
 
   const [blogs, setBlogs] = useState(null);
 
+  // Error Mesage set in UI 
+  const[error,setError]=useState(null);
+
   // Loading Logic
   const [isPending,setIsPending]=useState(true);
   
@@ -11,21 +14,29 @@ const Home = () => {
 useEffect(() => {
   fetch(' http://localhost:8000/blogs')
   .then(res=>{
+    if (!res.ok){
+      throw Error("Server side error in response");
+    }
+
     return res.json();
   })
   .then( data =>{
 console.log(data)
 setBlogs(data);
 setIsPending(false);
+setError(null)
 
 }).catch(err=>{
-  console.log(err.message)
+
+  setIsPending(false);
+  setError(err.message)
+  
 })
 
 },[])
   return (
     <div className="home">
-     
+{error && <div>{error}</div>}
 { isPending && <div>Loading...</div>}
  { blogs && <BlogList blogs={blogs} title="All Blogs" />}
   
